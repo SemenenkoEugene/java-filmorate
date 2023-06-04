6ae6b8ef0c25a802f3186c913cdc09beee129fe9
SemenenkoEugene <gks-08@mail.ru>
2023-06-04 21:19:21 +0300

    ver.3

--- src/main/java/ru/yandex/practicum/filmorate/exception/InternalServerErrorException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/InternalServerErrorException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class InternalServerErrorException extends RuntimeException{
+public class InternalServerErrorException extends RuntimeException {
     public InternalServerErrorException(String message) {
         super(message);
     }
